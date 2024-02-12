5b662036b8700bdc2e441e96ebfe81ff2dc53de5
mario.arista <mario.arista.dev@gmail.com>
2024-02-12 12:23:45 +0100

    fixing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 // Dichiaro Output
 const timerElement = document.getElementById("output");
 
-
-// Metto data lezione 'February 12, 2024 09:30:00'
-const dateLecture = new Date(2024, 1, 12, 9, 30, 0, 0); 
-console.log(dateLecture);
+// Metto data a caso 'Feb 12, 2024 09:30:00'
+const dateLecture = new Date(2024, 1, 12, 9, 30, 0, 0);
 
 // Mi chiamo la funzione
 function countdown() {
 
     // Differenza tra le due date
     let timeDifference = dateLecture - new Date();
-    // console.log(timeDifference);
 
     // converto i millesecondi
     let hours = Math.floor(timeDifference / (1000 * 60 * 60));
     let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-    let milliseconds = seconds / 1000;
 
      
-    timerElement.innerText = `${hours} ore, ${minutes} minuti, ${seconds} secondi, ${milliseconds} ms `;
+    timerElement.innerText = `${hours} ore, ${minutes} minuti, ${seconds} secondi. `;
+
+    if (timeDifference < 0) {
+        timerElement.innerText = "Tempo scaduto!";
+    }
 
 }
 
